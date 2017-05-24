becb1821668c92dce730102a8ae9d7786e41353e
Kaustav Banerjee <kaustavsmailbox21@gmail.com>
2017-05-24 04:34:16 -0700

    Add files via upload

--- networking assignments/client.py
+++ networking assignments/client.py
@@ -1,22 +1,17 @@
 import socket
 import sys
 
-# Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Connect the socket to the port where the server is listening
 server_address = ('localhost', 10000)
 sock.connect(server_address)
 try:
-    message=""
-    # Send data
-    while(message.count("exit")==0):
-	message=raw_input().strip()
-    	sock.sendall(message)
-	# Look for the response
-        data = sock.recv(4096)
-        print >>sys.stderr, 'Server Echo:  "%s"' % data
+        message=""
+        while(message.count("exit")==0):
+            message=raw_input().strip()
+            sock.sendall(message)
+            data = sock.recv(4096)
+            print >>sys.stderr, 'Server Echo:  "%s"' % data
 
 finally:
-    print >>sys.stderr, 'closing socket'
-    sock.close()
+        print >>sys.stderr, 'closing socket'
+        sock.close()
